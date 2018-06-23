f3f9931e2aedc01fcd7ec045c3730669101f6b2a
Cesar Rodriguez <kesarr@gmail.com>
2018-06-23 10:14:25 +0200

    material update

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { TranslateModule } from '@ngx-translate/core';
 import { PrettyJsonModule } from 'angular2-prettyjson';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { MasterpageComponent } from './masterpage/masterpage.component';
+import { HeaderComponent } from './masterpage/header/header.component';
+import { SidenavComponent } from './masterpage/sidenav/sidenav.component';
 import { PageComponent } from './page/page.component';
 import { LoadingComponent } from './page/loading/loading.component';
 import { NavbarComponent } from './page/navbar/navbar.component';
 import { SidebarComponent } from './page/sidebar/sidebar.component';
+import { TableComponent } from './table/table.component';
 
 import { ToKbPipes } from './pipes/tokb.pipes';
 
@@ -16,15 +28,29 @@ const sharedModules = [
   CommonModule,
   FormsModule,
   RouterModule,
+  FlexLayoutModule,
   TranslateModule,
   PrettyJsonModule
 ];
 
+const sharedMaterialModules = [
+  MatSidenavModule,
+  MatListModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatProgressSpinnerModule
+];
+
 const sharedComponents = [
+  MasterpageComponent,
+  HeaderComponent,
+  SidenavComponent,
   PageComponent,
   LoadingComponent,
   NavbarComponent,
-  SidebarComponent
+  SidebarComponent,
+  TableComponent
 ];
 
 const sharedPipes = [
@@ -33,7 +59,8 @@ const sharedPipes = [
 
 @NgModule({
   imports: [
-    ...sharedModules
+    ...sharedModules,
+    ...sharedMaterialModules
   ],
   declarations: [
     ...sharedComponents,
@@ -41,6 +68,7 @@ const sharedPipes = [
   ],
   exports: [
     ...sharedModules,
+    ...sharedMaterialModules,
     ...sharedComponents,
     ...sharedPipes
   ]
